f5ff07d751e9d3da6b5699eb39505ac048d613a9
Lucas Mumbarra <lucassantosm.2014@gmail.com>
2022-08-24 21:27:43 -0300

    Adicionado calculo da area da circunferencia

---
+++ aula1/areaCircunferencia.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    float pi, raio, area;
+    pi=3.1415625;
+    raio=5.5;
+    area=pi*raio*raio;
+    printf("o resultado:%f",area);
+    printf("\n");
+    return 0;
+}
