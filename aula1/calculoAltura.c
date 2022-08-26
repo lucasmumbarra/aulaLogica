5f53967c3493b7294668002da208a3cd7a8ef65f
sabrinafagundes <sabrina.fag@hotmail.com>
2022-08-25 21:12:27 -0300

    Adicionado calculo de altura

---
+++ aula1/calculoAltura.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+ 
+ //declaracao de variaveis
+ int idade;
+ float altura;
+ double salario;
+ char sexo;
+ 
+//atribuicao de valores
+idade = 20
+altura = 1.52
+salario = 2200
+sexo = 'F'
+
+// impressao de valores
+printf (" idade %d \n altura %.2f \n salario %.2f \n sexo %c",
+        idade, altura, salario, sexo);
+    
+printf("\n idade %d \t altura %.2f \t salario %.2f \t sexo %c",
+        idade, altura, salario, sexo);
+        
+return 0;
+
+
+
+}
